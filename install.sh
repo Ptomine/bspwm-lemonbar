3af6f1b7c3985104b231a3869afdceb0352e1f9b
ptomine <blinov592@gmail.com>
2018-07-01 16:02:16 +0000

    lemon improved, install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir -p ~/scripts
+cp bspwmrc ~/.config/bspwm/bspwmrc
+cp lemon.sh ~/scripts/lemon.sh
+cp sxhkdrc ~/.config/sxhkd/sxhkdrc
+cp .Xresources ~/.Xresources
--- lemon.sh
+++ lemon.sh
@@ -12,7 +12,6 @@ battery() {
 
 volume() {
 	amixer get Master | grep 'off' >/dev/null && echo "muted" || amixer get Master | grep -w 'Mono' | awk '{print$4}'
-	 #sed -n 'N;s/^.*\[\([0-9]\+%\).*$/\1/p'
 }
 
 cpuload() {
@@ -27,30 +26,56 @@ memused() {
 }
 
 network() {
-    # read lo int1 int2 <<< `ip link | sed -n 's/^[0-9]: \(.*\):.*$/\1/p'`
 	ip link show wlp3s0 | grep 'state UP' >/dev/null && (ping -c 1 8.8.8.8 >/dev/null 2>&1 && echo "" || echo "disconnected") || (ip link show enp0s25 | grep 'state UP' >/dev/null && (ping -c 1 8.8.8.8 >/dev/null 2>&1 && echo "" || echo "disconnected"))
-    # if iwconfig $int1 >/dev/null 2>&1; then
-        # wifi=$int1
-        # eth0=$int2
-    # else
-        # wifi=$int2
-        # eth0=$int1
-    # fi
-    # ip link show $eth0 | grep 'state UP' >/dev/null && (ping -c 1 8.8.8.8 >/dev/null 2>&1 && echo "$int " || echo "$int disconnected") || (ping -c 1 8.8.8.8 >/dev/null 2>&1 && echo "$int " || echo "$int disconnected")
+}
 
-    #int=eth0
-
-    # ping -c 1 8.8.8.8 >/dev/null 2>&1 && 
-        # echo "$int " || echo "$int "
+get_app_icon() {
+	if [[ ${1} == "\"sublime_text\"" ]] ; then
+        echo ""
+	elif [[ ${1} == "\"URxvt\"" ]] ; then
+	   echo ""
+	elif [[ ${1} == "\"Vimb\"" ]] ; then
+	   echo ""
+	elif [[ ${1} == "\"TelegramDesktop\"" ]] ; then
+	   echo ""
+	else
+	   echo ""
+	fi	
 }
 
 groups() {
     cur=`xprop -root _NET_CURRENT_DESKTOP | awk '{print $3}'`
     tot=`xprop -root _NET_NUMBER_OF_DESKTOPS | awk '{print $3}'`
 
-    for w in `seq 0 $((cur - 1))`; do line="${line}"; done
-    line="${line}"
-    for w in `seq $((cur + 2)) $tot`; do line="${line}"; done
+    for win_id in $( wmctrl -l | cut -d' ' -f1 ); do
+    	dt=`xprop -id $win_id _NET_WM_DESKTOP | awk '{print $3}'`
+    	class=`xprop -id $win_id WM_CLASS | cut -d" " -f4-`
+    	apps[$dt]="${apps[$dt]}$(get_app_icon ${class})"
+    done
+
+    for w in `seq 0 $((cur - 1))` ; do
+    	if [ -z ${apps[$w]} ] ; then
+    		line="${line}        ."
+    	else
+    		line="${line}    ${apps[$w]}    ."
+    	fi
+    done
+
+    if [ -z ${apps[$cur]} ] ; then
+    	line="${line}%{B#005577}%{+u}        %{B#000000}%{-u}"
+    else
+    	line="${line}%{B#005577}%{+u}    ${apps[$cur]}    %{B#000000}%{-u}"
+    fi
+
+    for w in `seq $((cur + 1)) $((tot - 1))` ; do
+    	if [ -z ${apps[$w]} ] ; then
+    		line="${line}.        "
+    	else
+    		line="${line}.    ${apps[$w]}    "
+    	fi
+    done
+
+    unset apps
     echo $line
 }
 
